ca9863f5a808bf75df947f98587755a012ed7bf5
ElsayedAhmed <hasoubaty@gmail.com>
2016-05-10 16:59:57 +0200

    adding Template and create layout

--- zendProject/application/Bootstrap.php
+++ zendProject/application/Bootstrap.php
@@ -9,17 +9,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 		
 		$view->headTitle('LikeLynda');
 
-		$view->headLink()->prependStylesheet('/ZendProject/zendProject/public/
-assets/css/bootstrap-theme.css');
-		$view->headLink()->prependStylesheet('/ZendProject/zendProject/public/
-assets/css/bootstrap.min.css');
-		$view->headLink()->prependStylesheet('/ZendProject/zendProject/public/
-assets/css/font-awesome.min.css');
-		$view->headLink()->appendStylesheet('/ZendProject/zendProject/public/
-assets/css/style.css');
+		$view->headLink()->prependStylesheet('/zendProject/ZendProject/zendProject/public/assets/css/bootstrap-theme.css');
+		$view->headLink()->prependStylesheet('/zendProject/ZendProject/zendProject/public/assets/css/bootstrap.min.css');
+		$view->headLink()->prependStylesheet('/zendProject/ZendProject/zendProject/public/assets/css/font-awesome.min.css');
+		$view->headLink()->appendStylesheet('/zendProject/ZendProject/zendProject/public/assets/css/style.css');
 
-		$view->headScript()->prependFile('/ZendProject/zendProject/public/
-assets/js/custom.js');
+		$view->headScript()->prependFile('/zendProject/ZendProject/zendProject/public/assets/js/custom.js');
 
 		$view->headMeta()->appendName('viewport', 'width=device-width, initial-scale=1.0');
 		$view->headMeta()->appendName('description', 'eLearning is a modern and fully responsive Template by WebThemez.');
